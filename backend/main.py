4d3ba6aa01c169e5cdcdd78e75a6080295c123b4
stackotter <stackotter@stackotter.dev>
2024-08-17 08:58:40 +1000

    backend: Create panorama viewing endpoint

--- backend/main.py
+++ backend/main.py
@@ -10,6 +10,43 @@ CORS(app)
 def index():
     return "Hello, world!"
 
+@app.route("/view/pano/<int:id>")
+def view_pano(id: int):
+    return """
+    <!DOCTYPE HTML>
+    <html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>A simple example</title>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/pannellum@2.5.6/build/pannellum.css"/>
+        <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/pannellum@2.5.6/build/pannellum.js"></script>
+        <style>
+            html, body {
+                margin: 0;
+            }
+
+            #panorama {
+                width: 100%%;
+                height: 100vh;
+            }
+        </style>
+    </head>
+    <body>
+
+    <div id="panorama"></div>
+    <script>
+    pannellum.viewer('panorama', {
+        "type": "equirectangular",
+        "panorama": "/pano/%d",
+        "autoLoad": true
+    });
+    </script>
+
+    </body>
+    </html>
+    """ % id
+
 @app.route("/pano/<int:id>")
 def pano(id: int):
     path = f"panos/{id}.jpg"
