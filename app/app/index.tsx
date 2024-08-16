2d7b0b7a32c4f5bbce91504714077d1d3635e5ea
stackotter <stackotter@stackotter.dev>
2024-08-16 21:13:06 +1000

    Continue setting up MapBox and add expo-build-properties

--- app/app/index.tsx
+++ app/app/index.tsx
@@ -1,15 +1,36 @@
-import { Text, View } from "react-native";
+import { StyleSheet, View } from "react-native";
+import React, { useEffect } from "react";
+import Mapbox, {MapView} from "@rnmapbox/maps";
+
+Mapbox.setAccessToken("pk.eyJ1Ijoic3RhY2tvdHRlciIsImEiOiJjbHp3amxuY24waG02MmpvZDhmN2QyZHQyIn0.j7bBcGFDFDhwrbzj6cgWQw");
+
+const styles = StyleSheet.create({
+  page: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#F5FCFF"
+  },
+  container: {
+    height: 300,
+    width: 300,
+    backgroundColor: "tomato"
+  },
+  map: {
+    flex: 1
+  }
+});
 
 export default function Index() {
+  useEffect(() => {
+    Mapbox.setTelemetryEnabled(false);
+  }, []);
+
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style={styles.page}>
+      <View style={styles.container}>
+        <MapView style={styles.map} />
+      </View>
     </View>
   );
 }
