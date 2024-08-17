e5aff3a4a0a8a6f71cf00e6deff8a13425ad0a1a
stackotter <stackotter@stackotter.dev>
2024-08-17 11:12:48 +1000

    imaging: Create imaging/data collection tooling, ready to start taking photos

---
+++ imaging/location_grabber.py
@@ -0,0 +1,68 @@
+import os
+import json
+
+from selenium import webdriver
+
+# https://maps.uq.edu.au/?zoom=16.580889752958626&campusId=406&lat=-27.4979649714223&lng=153.015831589828&zLevel=1
+
+driver = webdriver.Firefox()
+driver.get("https://maps.uq.edu.au/")
+driver.execute_script(
+    """
+    let div = document.createElement("div")
+    div.style.backgroundColor = "red";
+    div.style.width = "10px";
+    div.style.height = "10px";
+    div.style.position = "absolute";
+    div.style.top = "50%";
+    div.style.left = "50%";
+    div.style.transform = "translate(-50%, -50%)";
+    document.body.appendChild(div);
+    """
+)
+
+outfile = "latlongs.json"
+if not os.path.isfile(outfile):
+    with open(outfile, "w") as f:
+        f.write("[]")
+
+with open(outfile, "r") as f:
+    data = json.loads(f.read())
+
+floor = None
+while True:
+    try:
+        new_floor = input(f"Floor [default: {floor}]: " if floor is not None else "Floor: ")
+        if new_floor != "" or floor is None:
+            floor = new_floor
+
+        tags = [x.strip() for x in input("Tags: ").split(",") if x.strip() != ""]
+
+        connected_rooms = [x.strip() for x in input("Connected rooms: ").split(",") if x.strip() != ""]
+        connected_nodes = [int(x.strip()) for x in input("Connected nodes: ").split(",") if x.strip() != ""]
+
+        notes = input("Notes [optional]: ")
+        if notes == "":
+            notes = None
+
+        url = driver.current_url
+
+        lat = float(url.split("&lat=")[1].split("&")[0])
+        long = float(url.split("&lng=")[1].split("&")[0])
+
+        print("Done.\n")
+
+        data.append({
+            "lat": lat,
+            "lng": long,
+            "tags": tags,
+            "connected_rooms": connected_rooms,
+            "connected_nodes": connected_nodes,
+            "notes": notes
+        })
+        with open(outfile, "w") as f:
+            f.write(json.dumps(data))
+    except KeyboardInterrupt:
+        break
+    except Exception:
+        continue
---
+++ imaging/plot_nodes.py
@@ -0,0 +1,32 @@
+import folium
+import json
+
+from time import sleep
+from selenium import webdriver
+
+driver = webdriver.Firefox()
+driver.get("file:///Users/stackotter/Desktop/Projects/UQMapsReborn/uq-maps-reborn/imaging/map.html")
+
+firstTime = True
+while True:
+    map = folium.Map(
+        [-27.49999290407647, 153.01510439349107],
+        zoom_start=22
+    )
+
+    with open("latlongs.json", "r") as f:
+        nodes = json.loads(f.read())
+
+    for i, node in enumerate(nodes):
+        point = folium.Marker([node["lat"], node["lng"]], popup=f"Node {i}")
+        point.add_to(map)
+
+    map.save("map.html")
+
+    if firstTime:
+        driver.get("file:///Users/stackotter/Desktop/Projects/UQMapsReborn/uq-maps-reborn/imaging/map.html")
+        firstTime = False
+    else:
+        driver.refresh()
+
+    sleep(5)
