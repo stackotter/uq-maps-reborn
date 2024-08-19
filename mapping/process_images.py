e1538158d0d865199c11711514d843737dd1486b
stackotter <stackotter@stackotter.dev>
2024-08-20 00:56:07 +1000

    mapping: Blur faces in 360 imagery

--- mapping/process_images.py
+++ mapping/process_images.py
@@ -1,4 +1,6 @@
 import os
+import subprocess
+import shutil
 
 from PIL import Image, ImageDraw
 
@@ -12,4 +14,7 @@ for i, pano in enumerate(panos):
     with Image.open(pano) as im:
         draw = ImageDraw.Draw(im)
         draw.rectangle(((0, im.height - 200), (im.width, im.height)), fill="black")
-        im.save(os.path.join(outdir, f"{i}.jpg"))
+        outpath = os.path.join(outdir, f"{i}.jpg")
+        im.save(outpath)
+        subprocess.check_output(["/usr/bin/env", "deface", outpath])
+        shutil.move(os.path.join(outdir, f"{i}_anonymized.jpg"), outpath)
